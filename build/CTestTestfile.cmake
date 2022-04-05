582b0470f86d9b856597f2adb8b627698cf81eb2
masazumikatoh <gr.so.hi.na.92@me.com>
2022-04-05 22:26:28 +0900

    create ros environment

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/masazumi/ros_excercise/src
+# Build directory: /home/masazumi/ros_excercise/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
